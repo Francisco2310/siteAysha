30ea0e0220c60f7d35c6965ebc66cf58fddb9fd0
Francisco2310 <74802078+Francisco2310@users.noreply.github.com>
2025-10-03 06:38:56 -0300

    last sections added

--- src/script.js
+++ src/script.js
@@ -13,5 +13,9 @@ observer.observe(document.querySelector('.animate-polaroid2'));
 observer.observe(document.querySelector('.animate-polaroid3'));
 observer.observe(document.querySelector('.animate-rose'));
 observer.observe(document.querySelector('.animate-selo'));
-observer.observe(document.querySelector('animate-foto'));
-observer.observe(document.querySelector('animate-camera'))
+observer.observe(document.querySelector('.animate-camera'));
+observer.observe(document.querySelector('.animate-filme'));
+observer.observe(document.querySelector('.animate-knight'));
+observer.observe(document.querySelector('.animate-fita'));
+observer.observe(document.querySelector('.animate-fita2'));
+observer.observe(document.querySelector('.animate-fita3'));
